dc00af3de579608ce780257c7878ebef04dba724
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-02-29 17:14:52 -0800

    Make server listen to config port with 3000 as fallback

--- webApp/app.js
+++ webApp/app.js
@@ -4,8 +4,14 @@ const bodyParser = require('body-parser');
 const request = require("request");
 const app = express();
 
+// The port used for Express server
+const PORT = 3000;
+
+// Starts server
+app.listen(process.env.PORT || PORT, function() {
+  console.log('Bot is listening on port ' + PORT);
+});
+
 app.get('/', function(req, res){
    res.send("Hello world!");
 });
-
-app.listen(3000);

b966125da4d0cb39df216782f316ef2f8d7fa149
Jordan Martinez <jordanalex.martinez@gmail.com>
2020-02-29 17:20:51 -0800

    Add example post command for server from online tutorial

--- webApp/app.js
+++ webApp/app.js
@@ -15,6 +15,21 @@ app.listen(process.env.PORT || PORT, function() {
 // Add middlewares
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+// Routes
+app.post('/', (req, res) => {
+  var data = { form:
+                { token: process.env.SLACK_AUTH_TOKEN,
+                  channel: "#general",
+                  text: "Hi! :wave: \n I'm your new bot."
+                }
+             };
+  request.post('https://slack.com/api/chat.postMessage', data, function (error, response, body) {
+    // Sends welcome message
+    res.json();
+  });
+});
+
 app.get('/', function(req, res){
    res.send("Hello world!");
 });
